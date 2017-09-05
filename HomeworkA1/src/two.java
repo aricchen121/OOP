be2be1f56e39403af7e2036b7b3b8f13c72c9e60
Aric Chen <achen1917@gmail.com>
2017-09-05 11:14:55 -0400

    add homeworkA1

---
+++ HomeworkA1/src/two.java
@@ -0,0 +1,21 @@
+import java.math.*;
+import java.util.Scanner;
+public class two {
+
+	public static void main(String[] args) {
+		
+		Scanner input = new Scanner(System.in); 	// Create a new Scanner
+		
+		System.out.println("Enter ctrl z to exit");
+		System.out.print("Enter radius: ");
+		// User enter radius to find area 
+		while (input.hasNextDouble()){
+		double radius = input.nextDouble();
+		double area = Math.PI * Math.pow(radius, 2);
+		
+		System.out.println("Area is: " + area);
+		System.out.print("Enter radius: ");
+		}
+	}
+
+}
